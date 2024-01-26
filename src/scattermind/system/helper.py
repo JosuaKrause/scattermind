da8fa59b7de607ec51cf035cd110265fbc3f269d
Josua Krause <josua.krause@gmail.com>
2024-01-25 19:16:06 -0500

    finished docs for root

--- src/scattermind/system/helper.py
+++ src/scattermind/system/helper.py
@@ -23,10 +23,23 @@ T = TypeVar('T')
 
 
 class DictHelper(Generic[T]):
+    """
+    A sub-classable dictionary class with string keys.
+
+    Args:
+        Generic (_type_): The value type.
+    """
     def __init__(self, obj: dict[str, T] | None = None) -> None:
         self._obj = {} if obj is None else obj
 
     def set(self, /, key: str, value: T) -> None:
+        """
+        Set the value for the given key.
+
+        Args:
+            key (str): The key.
+            value (T): The value.
+        """
         self._obj[key] = value
 
     @overload
@@ -38,6 +51,17 @@ class DictHelper(Generic[T]):
         ...
 
     def get(self, /, key: str, default: T | None = None) -> T | None:
+        """
+        Return the value associated with the key or the default value if the
+        key does not exist.
+
+        Args:
+            key (str): The key.
+            default (T | None, optional): The default value. Defaults to None.
+
+        Returns:
+            T | None: The value associated with the key or the default value.
+        """
         return self._obj.get(key, default)
 
     def __getitem__(self, key: str) -> T:
@@ -47,12 +71,30 @@ class DictHelper(Generic[T]):
         self._obj[key] = value
 
     def items(self) -> Iterable[tuple[str, T]]:
+        """
+        Iterates through all items in the dictionary.
+
+        Yields:
+            tuple[str, T]: Key value pairs.
+        """
         yield from self._obj.items()
 
     def keys(self) -> Iterable[str]:
+        """
+        Iterates through all keys.
+
+        Yields:
+            str: The key.
+        """
         yield from self._obj.keys()
 
     def values(self) -> Iterable[T]:
+        """
+        Iterates through all values.
+
+        Yields:
+            T: The value.
+        """
         yield from self._obj.values()
 
     def __str__(self) -> str:
